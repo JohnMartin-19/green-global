08bf2eb4af543435593b59301d73c37673197e2f
John <johnnkonge2020@gmail.com>
2024-02-10 15:34:28 +0300

    modified app

---
+++ frontend/frontend/src/components/landing.jsx
@@ -0,0 +1,10 @@
+import {React} from "react";
+
+function LandingPage(){
+    return(
+        <div>
+            <h1 className="text-right mt-5">Green<span>Global</span></h1>
+        </div>
+    )
+}
+export default LandingPage
