c8a196933f5dc860f15e1e9bda1ade10f207d84e
John <johnnkonge2020@gmail.com>
2024-02-10 17:02:53 +0300

    fixing bugs

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,12 +1,46 @@
-import LandingPage from './components/landing';
-import './App.css';
+// filename -App.js
+
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
+
+import About from "./AboutUs";
+import Events from "./pages/events";
+import AnnualReport from "./pages/annual";
+import Teams from "./pages/team";
+import Blogs from "./pages/blogs";
+import SignUp from "./pages/signup";
+import LandingPage from "./components/landing";
 
 function App() {
-  return (
-    <div className="App">
-      <LandingPage/>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/about" element={<About />} />
+				<Route
+					path="/events"
+					element={<Events />}
+				/>
+				<Route
+					path="/annual"
+					element={<AnnualReport />}
+				/>
+				<Route path="/team" element={<Teams />} />
+				<Route path="/blogs" element={<Blogs />} />
+				<Route
+					path="/sign-up"
+					element={<SignUp />}
+				/>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
