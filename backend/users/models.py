23b101c7b76daf1dc977af3f0901760a0ecd9089
John <johnnkonge2020@gmail.com>
2024-02-16 16:00:39 +0300

    created users  models

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class  CustomUser(models.Model):
+    username = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(('email address'), unique=True)
+    phone = models.IntegerField(unique=True,max_length=13)
+    address = models.CharField(max_length=500)
+
+class Buyer(CustomUser):
+    orders = models.ForeignKey('Order', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.username
+class Farmer(CustomUser):
+    farm_name = models.CharField(max_length=64)
+    products = models.ForeignKey("Product",on_delete=models.CASCADE)
+    location = models.CharField(max_length=128) 
+
+    def __str__(self):
+        return self.farm_name + " - " + self.location
+    
