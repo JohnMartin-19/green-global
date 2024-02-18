37bddbfd39c7a84555f5db80740e3b5b2c651c0c
John <johnnkonge2020@gmail.com>
2024-02-18 13:26:08 +0300

    fixing bugs

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Products(models.Model):
+    product_id = models.UUIDField(primary_key=True,editable=False,default = uuid.uuid4)
+    product_name = models.CharField(max_length=100, null=False)
+    price = models.IntegerField(null=False)
+    quantity = models.CharField(max_length=50)  # can be a number or text like "dozen"
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_out_of_stock = models.BooleanField(default=False)
+   
+
+    def __str__(self):
+        return self.product_name + ' - KSH ' + str(self.price)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,22 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
-class  CustomUser(models.Model):
+import uuid
+# Create your models here
+class Buyer(models.Model):
+    buyer_id = models.UUIDField(primary_key=True,default = uuid.uuid4,editable = False)
     username = models.CharField(max_length=128)
     email = models.EmailField(('email address'), unique=True)
     phone = models.IntegerField(unique=True)
     address = models.CharField(max_length=500)
-
-    
-class Buyer(CustomUser):
     orders = models.CharField(max_length = 60)
     def __str__(self):
         return self.username
-class Farmer(CustomUser):
+class Farmer(models.Model):
+    farmer_id =models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=128)
+    email = models.EmailField(('email address'), unique=True)
+    phone = models.IntegerField(unique=True)
+    address = models.CharField(max_length=500)
     farm_name = models.CharField(max_length=64)
-
-    
     location = models.CharField(max_length=128) 
 
     def __str__(self):
