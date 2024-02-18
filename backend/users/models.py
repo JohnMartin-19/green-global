d703e9bbc199544c277274481bbdb9f2597180b4
John <johnnkonge2020@gmail.com>
2024-02-18 13:43:38 +0300

    updated the app

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,7 +4,7 @@ import uuid
 class Buyer(models.Model):
     buyer_id = models.IntegerField(primary_key=True,unique=True)
     username = models.CharField(max_length=128)
-    email = models.EmailField(('email address'), unique=True,default = False)
+    email = models.EmailField(('email address'), unique=True)
     phone = models.IntegerField(unique=True)
     address = models.CharField(max_length=500,null = True)
     orders = models.CharField(max_length = 60)
@@ -13,7 +13,7 @@ class Buyer(models.Model):
 class Farmer(models.Model):
     farmer_id =models.IntegerField(primary_key=True, editable=False)
     username = models.CharField(max_length=128)
-    email = models.EmailField(('email address'), unique=True,default= False)
+    email = models.EmailField(('email address'), unique=True)
     phone = models.IntegerField(unique=True)
     address = models.CharField(max_length=500,null = True)
     farm_name = models.CharField(max_length=64)
