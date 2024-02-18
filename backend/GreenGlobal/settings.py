3d3a20081c2fed507934819ca957296ecdc3cf6f
John <johnnkonge2020@gmail.com>
2024-02-18 11:49:48 +0300

    added the app in settings configuration

--- backend/GreenGlobal/settings.py
+++ backend/GreenGlobal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #local
     'users.apps.UsersConfig',
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.CustomUser'
