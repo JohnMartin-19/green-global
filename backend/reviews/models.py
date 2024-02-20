6e53e4cb7f5152614e69bd4c65bdb49a1c8b6c14
John <johnnkonge2020@gmail.com>
2024-02-20 15:19:06 +0300

    made migrations

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    review_id = models.IntegerField(primary_key=True)
+    product_id = models.ForeignKey('products.Product',on_delete = models.CASCADE)
+    user = models.ForeignKey( 'users.CustomUser', on_delete=models.CASCADE, null=True) 
+    review_message = models.CharField(max_length = 200)
+    rating = models.PositiveIntegerField(null = True)
+    review_date = models.DateTimeField(auto_now_add=True)
