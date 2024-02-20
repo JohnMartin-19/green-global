eb1288a10602dad377c908219b187b43ff026dfb
John <johnnkonge2020@gmail.com>
2024-02-20 10:54:39 +0300

    updated the order table and the product table

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from users.models import CustomUser
-from products.models import Product
+
 # Create your models here.
 class Order(models.Model):
     user = models.ForeignKey(CustomUser,on_delete = models.CASCADE)
-    product_id = models.ManyToManyField(Product, null=True, blank=True)
+    product_id = models.ManyToManyField('products.Product', blank=True)
     quantity = models.PositiveIntegerField()
     total_price = models.FloatField(default=0)
     time = models.TimeField(auto_now_add=True)
