f7f72c9adc66dea141f3793915f06890b212cd28
John <johnnkonge2020@gmail.com>
2024-02-23 18:50:41 +0300

    enabled permissions on the post app(crud)

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,12 +4,14 @@ from django.shortcuts import render
 from rest_framework import generics,permissions
 from  .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class ProductList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
