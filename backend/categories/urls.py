f90b3e8734cb20f45bc249f445b70bca7caee919
John <johnnkonge2020@gmail.com>
2024-02-23 17:09:24 +0300

    categories APIs done!

--- backend/categories/urls.py
+++ backend/categories/urls.py
@@ -3,5 +3,5 @@ from .views import CategoryList, CategoryDetail
 
 urlpatterns = [
     path('<int:pk>/', CategoryDetail.as_view(),name='category_detail'),
-    path('',CategoryList.as_view(), name='category_list'),
+    path('', CategoryList.as_view(), name='category_list'),
 ]
--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
