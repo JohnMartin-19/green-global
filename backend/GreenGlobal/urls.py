f1c3b5897686784518d1eb5c4f0ecd68e4ec84a2
John <johnnkonge2020@gmail.com>
2024-02-23 17:01:47 +0300

    configured categories urls

--- backend/GreenGlobal/urls.py
+++ backend/GreenGlobal/urls.py
@@ -4,7 +4,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('users.urls')),
+    path('api/v1/users/',include('users.urls')),
     path('api/v1/products/', include('products.urls')),
     path('api/v1/orders/',include('orders.urls')),
+    path('api/v1/categories/' ,include('categories.urls')),
 ]
---
+++ backend/categories/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CategoryList, CategoryDetail
+
+urlpatterns = [
+    path('<int:pk>/', CategoryDetail.as_view(),name='category_detail'),
+    path('',CategoryList.as_view(), name='category_list'),
+]
