640a11fdfb468e80e0bddb3c4f5656aa2ca04821
John <johnnkonge2020@gmail.com>
2024-02-23 16:53:57 +0300

    configured orders apis

--- backend/orders/serializers.py
+++ backend/orders/serializers.py
@@ -1,1 +1,14 @@
-from rest_framework im
+from rest_framework import serializers
+from .models import Order
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = (
+            'id', 
+            'user', 
+            'product_id',
+            'quantity',
+            'total_price',
+            'time'
+            )
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from  .models import Order
+from .serializers import OrderSerializer
+
+class OrderList(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
