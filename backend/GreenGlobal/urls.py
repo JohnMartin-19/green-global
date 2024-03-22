13ec4213b70bb99eaf6aa8e801bcc0a43a0c65b2
John <johnnkonge2020@gmail.com>
2024-03-22 14:58:57 +0300

    set up jwt token and the api is working

--- backend/GreenGlobal/urls.py
+++ backend/GreenGlobal/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include ('rest_framework.urls')), # for token authentication
@@ -10,7 +10,12 @@ urlpatterns = [
     path('api/v1/orders/',include('orders.urls')),
     path('api/v1/categories/' ,include('categories.urls')),
     path('api/v1/reviews/', include(('reviews.urls'))),
+
     #url path for auth
+    #JWT URLS
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path("api/v1/", include("dj_rest_auth.urls")), #for login
     path("api/v1/register/", # new
         include("dj_rest_auth.registration.urls")),
